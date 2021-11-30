768d68f94d3a6a5c1fd009af2cea38ff049c0864
arcxingye <864903500@qq.com>
2021-11-30 15:06:14 +0800

    防止串群

--- src/plugins/TRManager/__init__.py
+++ src/plugins/TRManager/__init__.py
@@ -19,60 +19,64 @@ async def admin_menu_(bot: Bot, event: Event, state: T_State):
 tr_menu = on_command("tr菜单", priority=5, permission=GROUP)
 @tr_menu.handle()
 async def tr_menu_(bot: Bot, event: Event):
-    result="服在线:/<服名>在线\n查全服:/全服在线\n查背包:/<服名> /inv <玩家名>\n查wiki:/wiki <内容>\n签到:/tr签到\n查询:/查积分\ntr商店:/tr商店\n排行:/积分排行\n注册(仅限私聊)"
-    await bot.send(
-        event=event,
-        message = result
-    )
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        result="服在线:/<服名>在线\n查全服:/全服在线\n查背包:/<服名> /inv <玩家名>\n查wiki:/wiki <内容>\n签到:/tr签到\n查询:/查积分\n商店:/tr商店\n排行:/积分排行\n注册(仅限私聊)"
+        await bot.send(
+            event=event,
+            message = result
+        )
 
 # 泰拉瑞亚wiki查询
 tr_wiki = on_command("/wiki", priority=5)
 @tr_wiki.handle()
 async def tr_wiki_(bot: Bot, event: Event, state: dict):
-    await bot.send(
-        event = event,
-        message = "https://terraria.fandom.com/zh/index.php?search="+urllib.parse.quote(str(event.message))
-    )
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        await bot.send(
+            event = event,
+            message = "https://terraria.fandom.com/zh/index.php?search="+urllib.parse.quote(str(event.message))
+        )
 
 # 单服在线查询
 tr_online = on_keyword(["/" + i + "在线" for i in server_alias_list], priority=4, permission=GROUP)
 @tr_online.handle()
 async def tr_online_(bot: Bot, event: Event):
-    server_online = (await SendTrRequest((str(event.get_message()).strip()).replace("/","").replace("在线",""), "cmd", "/playing"))
-    if server_online:
-        if server_online['status'] == '200':
-            server_online = (';'.join(server_online['response'])).replace(r"Online Players", "在线玩家")
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        server_online = (await SendTrRequest((str(event.get_message()).strip()).replace("/","").replace("在线",""), "cmd", "/playing"))
+        if server_online:
+            if server_online['status'] == '200':
+                server_online = (';'.join(server_online['response'])).replace(r"Online Players", "在线玩家")
+            else:
+                server_online = "响应错误~ err 2"
         else:
-            server_online = "响应错误~ err 2"
-    else:
-        server_online = "你要查询的服爆炸了~ err 1"
-    await tr_online.send(server_online)
+            server_online = "你要查询的服爆炸了~ err 1"
+        await tr_online.send(server_online)
 
 # 全服在线查询
 all_online = on_command("/全服在线", priority=5, permission=GROUP)
 @all_online.handle()
 async def all_online_(bot: Bot, event: Event):
-    all = ''
-    num = []
-    total = 0
-    for item in server_alias_list:
-        single_online = await SendTrRequest(item, "cmd", "/playing")
-        if single_online:
-            if single_online['status'] == '200':
-                if(len(single_online['response']) == 2):
-                    all += (item+">"+str(single_online['response'][0]) +":"+str(single_online['response'][1]) + "\n")
-                    num.append(re.findall('\d+', str(re.findall(r"Online Players (.*.+?)/", str(single_online['response']))))[0])
-                elif(len(single_online['response']) == 1):
-                    all += (item+">"+str(single_online['response'][0])+"没人~\n")
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        all = ''
+        num = []
+        total = 0
+        for item in server_alias_list:
+            single_online = await SendTrRequest(item, "cmd", "/playing")
+            if single_online:
+                if single_online['status'] == '200':
+                    if(len(single_online['response']) == 2):
+                        all += (item+">"+str(single_online['response'][0]) +":"+str(single_online['response'][1]) + "\n")
+                        num.append(re.findall('\d+', str(re.findall(r"Online Players (.*.+?)/", str(single_online['response']))))[0])
+                    elif(len(single_online['response']) == 1):
+                        all += (item+">"+str(single_online['response'][0])+"没人~\n")
+                    else:
+                        all += (item+">未响应所需内容~ err 3\n")
                 else:
-                    all += (item+">未响应所需内容~ err 3\n")
+                    all += (item+">响应错误~ err 2\n")
             else:
-                all += (item+">响应错误~ err 2\n")
-        else:
-            all += (item+">爆炸了~ err 1\n")
-    for i in range(0, len(num)):
-        total = total + int(num[i])
-    await all_online.send("当前全服在线总计"+str(total)+"人\n" + all.replace("Online Players ", "").replace("There are currently no players online.","").strip('\n'))
+                all += (item+">爆炸了~ err 1\n")
+        for i in range(0, len(num)):
+            total = total + int(num[i])
+        await all_online.send("当前全服在线总计"+str(total)+"人\n" + all.replace("Online Players ", "").replace("There are currently no players online.","").strip('\n'))
 
 # 单服执行指令
 single_exec = on_keyword(["/" + i for i in server_alias_list], priority=5, permission=GROUP)
@@ -80,13 +84,13 @@ single_exec = on_keyword(["/" + i for i in server_alias_list], priority=5, permi
 async def single_exec_(bot: Bot, event: Event):
     command=str(event.get_message()).split(" ")
     if len(command) > 1:
-        exec_cmd=''
-        i=0
-        for item in command:
-            if i>0:
-                exec_cmd+=item+" "
-            i+=1
         if VerifyPermissions((str(event.get_session_id()).split("_"))[1]):
+            exec_cmd=''
+            i=0
+            for item in command:
+                if i>0:
+                    exec_cmd+=item+" "
+                i+=1
             server_exec_result = (await SendTrRequest(command[0].replace("/",""), "cmd", exec_cmd.strip()))
             if server_exec_result:
                 if server_exec_result['status'] == '200':
@@ -141,44 +145,45 @@ tr_inv = on_keyword(
 async def tr_inv_(bot: Bot, event: Event):
     command=str(event.get_message()).split(" ")
     if len(command) > 2:
-        inv_server=command[0].replace("/","")
-        inv_name=command[2]
-        server_inv_result = await SendTrRequest(inv_server, "inv", inv_name)
-        if server_inv_result:
-            if server_inv_result['status'] == '200':
-                server_inv_result = server_inv_result['inventory']
-                s1 = []
-                s2 = []
-                s3 = []
-                for item in (str(server_inv_result).split(", ")):
-                    data = item.split(":")
-                    s1.append(data[0]),
-                    s2.append(data[1])
-                for item in s1:
-                    if item:
-                        tr_item_path="./data/images/tr_item/"+item+".png"
-                        if os.path.exists(tr_item_path):
-                            s3.append(tr_item_path)
+        if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+            inv_server=command[0].replace("/","")
+            inv_name=command[2]
+            server_inv_result = await SendTrRequest(inv_server, "inv", inv_name)
+            if server_inv_result:
+                if server_inv_result['status'] == '200':
+                    server_inv_result = server_inv_result['inventory']
+                    s1 = []
+                    s2 = []
+                    s3 = []
+                    for item in (str(server_inv_result).split(", ")):
+                        data = item.split(":")
+                        s1.append(data[0]),
+                        s2.append(data[1])
+                    for item in s1:
+                        if item:
+                            tr_item_path="./data/images/tr_item/"+item+".png"
+                            if os.path.exists(tr_item_path):
+                                s3.append(tr_item_path)
+                            else:
+                                s3.append((await get_wiki_img(item)))
                         else:
-                            s3.append((await get_wiki_img(item)))
+                            s3.append('')
+                    inv_result = await synInv(s3,s2,inv_name)
+                    if inv_result:
+                        test=str(os.path.abspath(inv_result)).replace(r'\\',r'/')
+                        server_inv_result = [{
+                            "type": "image",
+                            "data": {
+                                "file": "file:///"+test
+                            }
+                        }]
                     else:
-                        s3.append('')
-                inv_result = await synInv(s3,s2,inv_name)
-                if inv_result:
-                    test=str(os.path.abspath(inv_result)).replace(r'\\',r'/')
-                    server_inv_result = [{
-                        "type": "image",
-                        "data": {
-                            "file": "file:///"+test
-                        }
-                    }]
+                        server_inv_result = "查询处理失败，请重试"
                 else:
-                    server_inv_result = "查询处理失败，请重试"
+                    server_inv_result = "你要找的人在"+inv_server+"神隐了~ err 2"
             else:
-                server_inv_result = "你要找的人在"+inv_server+"神隐了~ err 2"
-        else:
-            server_inv_result = "你要执行的服爆炸了~ err 1"
-        await tr_inv.send(server_inv_result)
+                server_inv_result = "你要执行的服爆炸了~ err 1"
+            await tr_inv.send(server_inv_result)
 
 # 注册提示
 tr_reg_tip = on_command("注册", priority=4, permission=PRIVATE)
@@ -328,77 +333,80 @@ async def bandel_(bot: Bot, event: Event, state: T_State):
 sign_in = on_command("/tr签到", priority=5, permission=GROUP)
 @sign_in.handle()
 async def sign_in_(bot: Bot, event: Event, state: T_State):
-    result=await tr_sign_in(event.get_user_id())
-    if result:
-        if result[0][2]>=0:
-            time_dif=(datetime.now()-datetime.strptime(str(result[0][4]),"%Y-%m-%d")).days
-            if time_dif>0:
-                # 时间相差为1天时判定为连续签到
-                if time_dif==1:
-                    count=result[0][3]+1
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        result=await tr_sign_in(event.get_user_id())
+        if result:
+            if result[0][2]>=0:
+                time_dif=(datetime.now()-datetime.strptime(str(result[0][4]),"%Y-%m-%d")).days
+                if time_dif>0:
+                    # 时间相差为1天时判定为连续签到
+                    if time_dif==1:
+                        count=result[0][3]+1
+                    else:
+                        count=0
+                    if count<3:
+                        add_score=random.randint(1,3)
+                    elif count==3:
+                        add_score=random.randint(2,4)
+                    elif count>=5:
+                        add_score=random.randint(3,5)
+                    elif count>=7:
+                        add_score=random.randint(4,6)
+                    score_add=await tr_update_score(event.get_user_id(),int(result[0][2])+add_score,count)
+                    if score_add:
+                        await sign_in.send("签到成功，积分+"+str(add_score)+"\n现在拥有积分"+str(int(result[0][2])+add_score)+"\n已连续签到"+str(count)+"天")
+                    else:
+                        await sign_in.send("签到失败")
                 else:
-                    count=0
-                if count<3:
-                    add_score=random.randint(1,3)
-                elif count==3:
-                    add_score=random.randint(2,4)
-                elif count>=5:
-                    add_score=random.randint(3,5)
-                elif count>=7:
-                    add_score=random.randint(4,6)
-                score_add=await tr_update_score(event.get_user_id(),int(result[0][2])+add_score,count)
-                if score_add:
-                    await sign_in.send("签到成功，积分+"+str(add_score)+"\n现在拥有积分"+str(int(result[0][2])+add_score)+"\n已连续签到"+str(count)+"天")
-                else:
-                    await sign_in.send("签到失败")
+                    await sign_in.send("你已经签到过了~请明天再来叭")
             else:
-                await sign_in.send("你已经签到过了~请明天再来叭")
+                await sign_in.send("获取账户失败")
         else:
-            await sign_in.send("获取账户失败")
-    else:
-        score_add=await tr_create_score(event.get_user_id(),1,0)
-        if score_add:
-            await sign_in.send("签到成功，积分+1\n现在拥有1积分")
-        else:
-            await sign_in.send("签到失败")
+            score_add=await tr_create_score(event.get_user_id(),1,0)
+            if score_add:
+                await sign_in.send("签到成功，积分+1\n现在拥有1积分")
+            else:
+                await sign_in.send("签到失败")
 
 # 查询积分
 query_score = on_command("/查积分", priority=5, permission=GROUP)
 @query_score.handle()
 async def query_score_(bot: Bot, event: Event, state: T_State):
-    if len(str(event.get_message()))==0:
-        result=await tr_sign_in(int(event.get_user_id()))
-        if result:
-            if result[0][2]>=0:
-                await query_score.send("你现在拥有"+str(result[0][2])+"积分\n已连续签到"+str(result[0][3])+"天")
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        if len(str(event.get_message()))==0:
+            result=await tr_sign_in(int(event.get_user_id()))
+            if result:
+                if result[0][2]>=0:
+                    await query_score.send("你现在拥有"+str(result[0][2])+"积分\n已连续签到"+str(result[0][3])+"天")
+                else:
+                    await query_score.send("获取账户失败")
             else:
-                await query_score.send("获取账户失败")
-        else:
-            await query_score.send("未找到账户，先来签个到吧。/tr签到")
-    elif len(str(event.get_message()))>0:
-        result=await tr_sign_in(int(str(event.get_message())))
-        if result:
-            if result[0][2]>=0:
-                await query_score.send("TA现在拥有"+str(result[0][2])+"积分\n已连续签到"+str(result[0][3])+"天")
+                await query_score.send("未找到账户，先来签个到吧。/tr签到")
+        elif len(str(event.get_message()))>0:
+            result=await tr_sign_in(int(str(event.get_message())))
+            if result:
+                if result[0][2]>=0:
+                    await query_score.send("TA现在拥有"+str(result[0][2])+"积分\n已连续签到"+str(result[0][3])+"天")
+                else:
+                    await query_score.send("获取账户失败")
             else:
-                await query_score.send("获取账户失败")
-        else:
-            await query_score.send("未找到账户")
+                await query_score.send("未找到账户")
 
 # 积分排行榜
 score_top = on_command("/积分排行", priority=5, permission=GROUP)
 @score_top.handle()
 async def score_top_(bot: Bot, event: Event, state: T_State):
-    result=await tr_score_top()
-    if result:
-        msg=""
-        i=1
-        for item in result:
-            msg+=str(i)+"."+str(item[1])+"->"+str(item[2])+"\n"
-            i+=1
-        await score_top.send(msg)
-    else:
-        await score_top.send("获取失败")
+    if VerifyTrGroup((str(event.get_session_id()).split("_"))[1]):
+        result=await tr_score_top()
+        if result:
+            msg=""
+            i=1
+            for item in result:
+                msg+=str(i)+"."+str(item[1])+"->"+str(item[2])+"\n"
+                i+=1
+            await score_top.send(msg)
+        else:
+            await score_top.send("获取失败")
 
 # 设置积分
 set_score = on_command("/设置积分", priority=5, permission=GROUP)
@@ -417,44 +425,46 @@ async def set_score_(bot: Bot, event: Event, state: T_State):
 tr_shop = on_command("/tr商店", priority=5, permission=GROUP)
 @tr_shop.handle()
 async def tr_shop_(bot: Bot, event: Event):
-    title="[兑换商店]\n"
-    goods=''
-    for item in shop_list:
-        goods+=str(item[0])+"."+item[1]+str(item[3])+"个 "+str(item[4])+"分\n"
-    tips="\n完整命令/tr兑换 <服名> <角色> <商品前面数字>\n请确保角色在线不然分扣了物品也没得到("
-    await tr_shop.send(title+goods+tips)
+    if VerifyPermissions((str(event.get_session_id()).split("_"))[1]):
+        title="[兑换商店]\n"
+        goods=''
+        for item in shop_list:
+            goods+=str(item[0])+"."+item[1]+str(item[3])+"个 "+str(item[4])+"分\n"
+        tips="\n完整命令/tr兑换 <服名> <角色> <商品前面数字>\n请确保角色在线不然分扣了物品也没得到("
+        await tr_shop.send(title+goods+tips)
 
 # tr兑换
 tr_buy = on_command("/tr兑换", priority=5, permission=GROUP)
 @tr_buy.handle()
 async def tr_buy_(bot: Bot, event: Event):
-    command=str(event.get_message()).strip().split(" ")
-    if len(str(command)) > 3:
-        server=command[0]
-        username=command[1]
-        good=command[2]
-        item_id=shop_list[int(good)-1][2]
-        cost=shop_list[int(good)-1][4]
-        num=shop_list[int(good)-1][3]
-        # 查询积分
-        score_result=await tr_sign_in(event.get_user_id())
-        if score_result:
-            if score_result[0][2]-cost>=0:
-                # 扣除积分
-                delete_result=await tr_delete_score(int(event.get_user_id()),score_result[0][3]-cost)
-                if delete_result:
-                    # 执行给物品指令
-                    exec_result=await SendTrRequest(server,"cmd","/give "+str(item_id)+" "+str(username)+" "+str(num))
-                    if exec_result:
-                        if exec_result['response'][0][0:4]=="Gave":
-                            await tr_buy.send("已发放物品")
+    if VerifyPermissions((str(event.get_session_id()).split("_"))[1]):
+        command=str(event.get_message()).strip().split(" ")
+        if len(str(command)) > 3:
+            server=command[0]
+            username=command[1]
+            good=command[2]
+            item_id=shop_list[int(good)-1][2]
+            cost=shop_list[int(good)-1][4]
+            num=shop_list[int(good)-1][3]
+            # 查询积分
+            score_result=await tr_sign_in(event.get_user_id())
+            if score_result:
+                if score_result[0][2]-cost>=0:
+                    # 扣除积分
+                    delete_result=await tr_delete_score(int(event.get_user_id()),score_result[0][3]-cost)
+                    if delete_result:
+                        # 执行给物品指令
+                        exec_result=await SendTrRequest(server,"cmd","/give "+str(item_id)+" "+str(username)+" "+str(num))
+                        if exec_result:
+                            if exec_result['response'][0][0:4]=="Gave":
+                                await tr_buy.send("已发放物品")
+                            else:
+                                await tr_buy.send("未成功发放，花积分买了个教训(")
                         else:
-                            await tr_buy.send("未成功发放，花积分买了个教训(")
+                            await tr_buy.send("服不存在或裂开了，积分花了个寂寞(")
                     else:
-                        await tr_buy.send("服不存在或裂开了，积分花了个寂寞(")
+                        await tr_buy.send("积分操作失败")
                 else:
-                    await tr_buy.send("积分操作失败")
+                    await tr_buy.send("积分不足")
             else:
-                await tr_buy.send("积分不足")
-        else:
-            await tr_buy.send("未获取到积分数据")
+                await tr_buy.send("未获取到积分数据")
