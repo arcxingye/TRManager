53897f0b4b6fab9db57935c85522567c4466bc58
arcxingye <864903500@qq.com>
2021-11-30 15:06:07 +0800

    新增tr群设置

--- src/plugins/TRManager/config.py
+++ src/plugins/TRManager/config.py
@@ -1,4 +1,6 @@
-# TR管理群，在这些群里的人才能用管理命令
+# TR群，填入服务器的群，只有这些群能用本插件普通指令
+TR_GROUP=[180093493,399915856,728833061]
+# TR管理群(只能再这个群使用服管命令)，管理群不用填上边，只填下面就行
 TR_ADMIN_GROUP=[12345,54321]
 
 # 商店配置：(唯一列表id，物品名，物品id，物品数量，兑换所需分数)
@@ -10,12 +12,13 @@ shop_list=[
 
 # 服务器列表：(服名，服ip，服Rest端口，服RestToken)
 # 可无限添加服务器，续写一定要注意格式
+# 没事可别截图，防止token泄露
 server_list=[
     ("k77","k.thac.cc","64212","token"),
     ("s77","s.thac.cc","12345","test")
 ]
 
-# 下面不用管--------------
+# 下面不用管-----------------------------
 # 服名称列表:
 server_alias_list=[]
 for item in server_list:
@@ -25,9 +28,15 @@ def get_server_info(alias:str,info:int):
     for item in server_list:
         if alias==item[0]:
             return item[info]
-# 验证权限
-def VerifyPermissions(tr_admin: str):
+# 验证是否TR群
+def VerifyTrGroup(group:str):
+    for item in TR_GROUP+TR_ADMIN_GROUP:
+        if int(group) == item:
+            return True
+    return False
+# 验证权限群
+def VerifyPermissions(group: str):
     for item in TR_ADMIN_GROUP:
-        if int(tr_admin) == item:
+        if int(group) == item:
             return True
     return False
