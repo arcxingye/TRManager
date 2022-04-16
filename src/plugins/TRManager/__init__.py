1843d7972167ad87f1a298097042040a3bafa09a
arcxingye <58928862+arcxingye@users.noreply.github.com>
2022-04-16 20:27:52 +0800

    nb2新版本适配

--- src/plugins/TRManager/__init__.py
+++ src/plugins/TRManager/__init__.py
@@ -1,6 +1,6 @@
 from nonebot import on_keyword, on_command
-from nonebot.adapters.cqhttp import Bot, Event, PRIVATE, GROUP
-from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+from nonebot.adapters.onebot.v11.permission import PRIVATE, GROUP
 import urllib,os,re,random,math
 from datetime import datetime
 from .model import *
